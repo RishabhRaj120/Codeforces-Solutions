bdf2f441a3eab2e7c5016796f01f8f722f05a376
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-05-30 00:40:24 +0530

    Adding Solution

---
+++ 0800/2148A_Sublime_Squence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <set>
+#include <tuple>
+#include <map>
+#include <bitset>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const double PI = 3.14159265358979323846;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        int x = 0;
+        int n = 0;
+        cin >> x >> n;
+        if (n & 1)
+        {
+            cout << x << '\n';
+        }
+        else
+        {
+            cout << "0\n";
+        }
+    }
+}
