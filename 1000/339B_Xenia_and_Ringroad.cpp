e1be8d6f646f207fe36955296de92ba11dab9db3
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-06-17 22:24:27 +0530

    Solution for 339B_Xenia_and_Ringroad (1000 Rated)

---
+++ 1000/339B_Xenia_and_Ringroad.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <set>
+#include <tuple>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const double PI = 3.14159265358979323846;
+string pi = "314159265358979323846264338327950288419716939937510";
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n = 0;
+    ll m = 0;
+    cin >> n >> m;
+    vector<ll> v1;
+    for (int i = 0; i < m; i++)
+    {
+        ll x = 0;
+        cin >> x;
+        v1.push_back(x);
+    }
+    ll c1 = 0;
+    c1 += (v1[0] - 1);
+    for (int i = 1; i < m; i++)
+    {
+        if (v1[i] < v1[i - 1])
+        {
+            c1 += (n - v1[i - 1]);
+            c1 += v1[i];
+        }
+        else
+        {
+            c1 += (v1[i] - v1[i - 1]);
+        }
+    }
+    cout << c1 << '\n';
+}
