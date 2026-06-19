9a7f504fd6b658e9d0a22feca9abb7002dc96a0a
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-06-19 23:48:00 +0530

    Solution for 1537B_Bad_Boy (900 Rated)

---
+++ 0900/1537B_Bad_Boy.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <set>
+#include <tuple>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const double PI = 3.14159265358979323846;
+string pi = "314159265358979323846264338327950288419716939937510";
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        ll n = 0;
+        ll m = 0;
+        ll i = 0;
+        ll j = 0;
+        cin >> n >> m >> i >> j;
+        cout << n << " " << m << " " << "1 " << "1\n";
+    }
+}
