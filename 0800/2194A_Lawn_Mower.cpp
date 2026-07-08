dbb16477171337dd4b00ec8289ec5b4c20eea109
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-07-08 22:01:32 +0530

    Solution for 2194A_Lawn_Mover (800 Rated)

---
+++ 0800/2194A_Lawn_Mower.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <set>
+#include <tuple>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const double PI = 3.14159265358979323846;
+string pi = "314159265358979323846264338327950288419716939937510";
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        ll n = 0;
+        ll w = 0;
+        cin >> n >> w;
+        if (w != 1)
+        {
+            cout << n - (n / w) << '\n';
+        }
+        else
+        {
+            cout << "0\n";
+        }
+    }
+}
