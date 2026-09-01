fae081399917ac0a134ff87355cc4adaeb528947
Ri_Rii <88502027+RishabhRaj120@users.noreply.github.com>
2026-09-01 21:38:08 +0530

    Solution for 1777A_Everybody_Likes_Good_Arrays! (800 Rated)

---
+++ 0800/1777A_Everybody_Likes_Good_Arrays!.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+#include <set>
+#include <tuple>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const double PI = 3.14159265358979323846;
+string pi = "314159265358979323846264338327950288419716939937510";
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        int n = 0;
+        cin >> n;
+        vector<ll> v1;
+        for (int i = 0; i < n; i++)
+        {
+            ll x = 0;
+            cin >> x;
+            v1.push_back(x);
+        }
+        int c1 = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (((v1[i] & 1) && (v1[i - 1] & 1)) || ((!(v1[i] & 1)) && (!(v1[i - 1] & 1))))
+            {
+                ++c1;
+            }
+        }
+        cout << c1 << '\n';
+    }
+}
